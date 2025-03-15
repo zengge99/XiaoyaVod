0c6f78a92f76e518b9e675e822b3f827336774a0
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-16 01:31:35 +0800

    ok

--- app/src/main/java/com/github/catvod/bean/alist/Item.java
+++ app/src/main/java/com/github/catvod/bean/alist/Item.java
@@ -107,13 +107,15 @@ public class Item {
     }
 
     public boolean isMedia(boolean isNew) {
-        if (getName().endsWith(".ts") || getName().endsWith(".mpg")) return true;
+        //if (getName().endsWith(".ts") || getName().endsWith(".mpg")) return true;
+        if (util.isMedia(getName())) return true;
         if (isNew) return getType() == 2 || getType() == 3;
         return getType() == 3 || getType() == 4;
     }
 
     public boolean ignore(boolean isNew) {
-        if (getName().endsWith(".ts") || getName().endsWith(".mpg")) return false;
+        //if (getName().endsWith(".ts") || getName().endsWith(".mpg")) return false;
+        if (util.isMedia(getName())) return false;
         if (isNew) return getType() == 0 || getType() == 4;
         return getType() == 0 || getType() == 2 || getType() == 5;
     }
