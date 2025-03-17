563f656c824dec44862cb8dbb51df90ac34a78f1
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-17 22:07:07 +0800

    保存用户名密码

--- app/src/main/java/com/github/catvod/spider/AList.java
+++ app/src/main/java/com/github/catvod/spider/AList.java
@@ -51,6 +51,7 @@ import java.util.stream.Collectors;
 import com.github.catvod.bean.alist.LoginDlg;
 import android.widget.Toast;
 import com.github.catvod.utils.Path;
+import java.io.File;
 
 public class AList extends Spider {
 
@@ -524,7 +525,7 @@ public class AList extends Spider {
             Logger.log("用户名:" + userName + "密码:" + password);
             userName = userName.isEmpty() ? "guest" : userName;
             password = password.isEmpty() ? "guest_Api789" : password;
-            String loginPath = Path.root() + "/" + getServer().replace("://", "_").replace(":", "_") + ".login";
+            String loginPath = Path.root() + "/" + drive.getServer().replace("://", "_").replace(":", "_") + ".login";
             File loginFile = new File(loginPath);
             Path.write(loginFile, (userName + "\n" + password).getBytes());
             params.put("username", userName);
@@ -545,7 +546,7 @@ public class AList extends Spider {
     private boolean loginByFile(Drive drive) {
         try {
             JSONObject params = new JSONObject();
-            String loginPath = Path.root() + "/" + getServer().replace("://", "_").replace(":", "_") + ".login";
+            String loginPath = Path.root() + "/" + drive.getServer().replace("://", "_").replace(":", "_") + ".login";
             File loginFile = new File(loginPath);
             String login = Path.read(loginFile) + "\n" + "\n";
             String userName = login.split("\n")[0];
