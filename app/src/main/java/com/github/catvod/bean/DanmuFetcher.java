e696b202c1742b5a00bb899641bac66a3ba6ff19
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-19 21:34:37 +0800

    测试弹幕库

--- app/src/main/java/com/github/catvod/bean/DanmuFetcher.java
+++ app/src/main/java/com/github/catvod/bean/DanmuFetcher.java
@@ -11,6 +11,7 @@ import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.github.catvod.spider.Logger;
 
 public class DanmuFetcher {
 
