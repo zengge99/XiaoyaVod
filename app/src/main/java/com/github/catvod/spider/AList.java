5b390a6e2d34dcfc7c5889af7438fe22d0512536
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-24 15:45:38 +0800

    重构

--- app/src/main/java/com/github/catvod/spider/AList.java
+++ app/src/main/java/com/github/catvod/spider/AList.java
@@ -808,7 +808,6 @@ public class AList extends Spider {
                 LocalIndexService service = LocalIndexService.get(drive.getServer());
                 return service.slim(drive.getPath());
             }
-            return new ArrayList<>();
         }
     }
 }
