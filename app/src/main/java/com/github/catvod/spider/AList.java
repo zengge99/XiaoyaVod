e651d645810cddf426cd8cd1517eddaa02e95940
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-24 21:42:54 +0800

    Update AList.java

--- app/src/main/java/com/github/catvod/spider/AList.java
+++ app/src/main/java/com/github/catvod/spider/AList.java
@@ -217,11 +217,13 @@ public class AList extends Spider {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
             }
+            Notify.show("开始构建本地索引，需要数秒");
             for (Drive d : drives) {
                 if (!d.noPoster() || d.search()) {
                     LocalIndexService.get(d).slim(d.getPath());
                 }
             }
+            Notify.show("构建本地索引完成");
         });
         thread.start();
         
