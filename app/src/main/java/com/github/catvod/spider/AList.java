80b5508c51d723271231319ae05ef9f13392b9b9
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-26 18:10:16 +0800

    Update AList.java

--- app/src/main/java/com/github/catvod/spider/AList.java
+++ app/src/main/java/com/github/catvod/spider/AList.java
@@ -833,9 +833,13 @@ public static List<String> doFilter(LocalIndexService service, HashMap<String, S
             }
             shortKeyword = shortKeyword.length() < 30 ? shortKeyword : shortKeyword.substring(0, 30);
             if (keyword.startsWith("~daily:")) {
-                return LocalIndexService.get(drive.getName() + "/"+ drive.dailySearchApi(shortKeyword)).query(new LinkedHashMap<String, String>());
+                LocalIndexService service = LocalIndexService.get(drive.getName() + "/"+ drive.dailySearchApi(shortKeyword));
+                service.slim(drive.getPath());
+                return service.query(new LinkedHashMap<String, String>());
             } else if (keyword.startsWith("~search:")) {
-                return LocalIndexService.get(drive.getName() + "/"+ drive.searchApi(shortKeyword)).query(new LinkedHashMap<String, String>());
+                LocalIndexService service = LocalIndexService.get(drive.getName() + "/"+ drive.searchApi(shortKeyword));
+                service.slim(drive.getPath());
+                return service.query(new LinkedHashMap<String, String>());
             } else if (keyword.startsWith("~quick:")) {
                 return LocalIndexService.get(drive).quickSearch(shortKeyword);
             } else {
