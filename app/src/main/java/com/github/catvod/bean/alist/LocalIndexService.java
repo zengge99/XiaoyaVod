21952fda4dda99a7a67f0817c000e1315bdea7c3
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-28 15:44:02 +0800

    ok

--- app/src/main/java/com/github/catvod/bean/alist/LocalIndexService.java
+++ app/src/main/java/com/github/catvod/bean/alist/LocalIndexService.java
@@ -14,6 +14,7 @@ import com.github.catvod.bean.alist.Drive;
 import com.github.catvod.bean.alist.Item;
 import com.github.catvod.bean.Vod;
 import java.nio.file.Paths;
+import java.nio.file.Files;
 
 public class LocalIndexService {
 
