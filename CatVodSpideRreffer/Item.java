a16e8122fd00b9175a37e07e8aa907b4d13d1702
zengge99 <zengge96@gmail.com>
2025-03-30 17:40:26 +0800

    Initial commit

---
+++ CatVodSpideRreffer/Item.java
@@ -0,0 +1,141 @@
+package com.github.catvod.bean.alist;
+
+import android.text.TextUtils;
+
+import com.github.catvod.bean.Vod;
+import com.github.catvod.bean.DoubanInfo;
+import com.github.catvod.utils.Util;
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.time.Instant;
+
+public class Item {
+
+    @SerializedName("name")
+    private String name;
+    @SerializedName(value = "path", alternate = "parent")
+    private String path;
+    @SerializedName("type")
+    private int type;
+    @SerializedName("size")
+    private long size;
+    @SerializedName(value = "thumb", alternate = "thumbnail")
+    private String thumb;
+    @SerializedName(value = "url", alternate = "raw_url")
+    private String url;
+    @SerializedName(value = "modified", alternate = "updated_at")
+    private String modified;
+    public DoubanInfo doubanInfo = new DoubanInfo();
+
+    public static Item objectFrom(String str) {
+        return new Gson().fromJson(str, Item.class);
+    }
+
+    public static List<Item> arrayFrom(String str) {
+        Type listType = new TypeToken<List<Item>>() {}.getType();
+        return new Gson().fromJson(str, listType);
+    }
+
+    public String getName() {
+        return TextUtils.isEmpty(name) ? "" : name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return TextUtils.isEmpty(path) ? "" : path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public String getThumb() {
+        return TextUtils.isEmpty(thumb) ? "" : thumb;
+    }
+
+    public void setThumb(String thumb) {
+        this.thumb = thumb;
+    }
+
+    public String getUrl() {
+        return TextUtils.isEmpty(url) ? "" : url.startsWith("//") ? "http:" + url : url;
+    }
+
+    public String getModified() {
+        return modified;
+    }
+
+    public Date getDate() {
+        try {
+            //SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault());
+            //return format.parse(getModified().subs
+            Instant instant = Instant.parse(getModified());
+            return Date.from(instant);
+        } catch (Exception e) {
+            return new Date();
+        }
+    }
+
+    public boolean isFolder() {
+        return getType() == 1;
+    }
+
+    public boolean isMedia() {
+        return Util.isMedia(getName());
+    }
+
+    public boolean ignore() {
+        if (isFolder()) return false;
+        return !Util.isMedia(getName());
+    }
+
+    public String getExt() {
+        return Util.getExt(getName());
+    }
+
+    public String getVodId(String id) {
+        return id + getPath() + "/" + getName();
+    }
+
+    public String getPic(String pic) {
+        return getThumb().isEmpty() && isFolder() ? pic : getThumb();
+    }
+
+    public String getRemark() {
+        return Util.getSize(getSize());
+    }
+
+    public Vod getVod(String id, String pic) {
+        return new Vod(getVodId(id), getName(), getPic(pic), getRemark(), isFolder());
+    }
+
+    public Vod getVod(Drive drive, String pic) {
+        return new Vod(getVodId(drive.getName()), getName(), getPic(pic), drive.getName(), isFolder());
+    }
+}
---
+++ CatVodSpideRreffer/KanDanmuFetcher.java
@@ -0,0 +1,114 @@
+package com.github.catvod.bean;
+ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+import com.github.catvod.spider.Logger;
+import com.github.catvod.utils.Path;
+import java.io.File;
+
+public class KanDanmuFetcher extends DanmuFetcher {
+
+    private static KanDanmuFetcher thisObject = new KanDanmuFetcher();
+
+    /**
+     * 获取 Bilibili 弹幕格式的 XML
+     *
+     * @param title   影片名
+     * @param episode 集数
+     * @param year    年份
+     * @return Bilibili 弹幕格式的 XML 字符串
+     * @throws IOException 如果请求失败
+     */
+    public static String getBilibiliDanmakuXML(String title, int episode, int year) {
+        try {
+            // Step 1: Get showId
+            String showId = thisObject.searchEnId(title, year);
+            if (showId == null) {
+                throw new RuntimeException("No matching show found");
+            }
+
+            // Step 2: Get episode URL
+            String episodeUrl = thisObject.getEpisodeUrl(showId, episode);
+            if (episodeUrl == null) {
+                throw new RuntimeException("No matching episode found");
+            }
+
+            // Step 3: Fetch danmaku data
+            List<List<Object>> danmakuData = thisObject.fetchDanmaku(episodeUrl);
+            if (danmakuData == null) {
+                throw new RuntimeException("Failed to fetch danmaku");
+            }
+
+            // Step 4: Convert to Bilibili XML format
+            return thisObject.convertToBilibiliXML(danmakuData);
+        } catch (Exception e) {
+            Logger.log(e);
+            return "";
+        }
+    }
+
+    private String searchEnId(String title, int year) throws IOException {
+        // URL 编码影片名
+        String encodedTitle = URLEncoder.encode(title, StandardCharsets.UTF_8.toString());
+        // 使用步骤1的API搜索节目ID
+        String searchUrl = "https://api.so.360kan.com/index?force_v=1&from=&pageno=1&v_ap=1&tab=all&kw=" + encodedTitle;
+        String jsonResponse = sendGetRequest(searchUrl);
+
+        Gson gson = new Gson();
+        JsonObject response = gson.fromJson(jsonResponse, JsonObject.class);
+
+        // 解析 JSON 结构
+        JsonObject data = response.getAsJsonObject("data");
+        JsonObject longData = data.getAsJsonObject("longData");
+        JsonArray rows = longData.getAsJsonArray("rows");
+
+        for (JsonElement item : rows) {
+            JsonObject show = item.getAsJsonObject();
+            String showYear = show.get("year").getAsString();
+            if (showYear.equals(String.valueOf(year))) {
+                return show.get("en_id").getAsString(); // 返回 en_id
+            }
+        }
+        return null;
+    }
+
+    private String getEpisodeUrl(String enId, int episode) throws IOException {
+        // 使用步骤2的API获取剧集URL，en_id 作为参数
+        String episodeUrl = "https://api.web.360kan.com/v1/detail?cat=2&id=" + enId;
+        String jsonResponse = sendGetRequest(episodeUrl);
+
+        Gson gson = new Gson();
+        JsonObject response = gson.fromJson(jsonResponse, JsonObject.class);
+        JsonObject allepidetail = response.getAsJsonObject("data").getAsJsonObject("allepidetail");
+
+        // 遍历 allepidetail 的所有字段
+        for (String key : allepidetail.keySet()) {
+            JsonArray episodes = allepidetail.getAsJsonArray(key); // 获取当前字段的 JsonArray
+            for (JsonElement item : episodes) {
+                JsonObject episodeData = item.getAsJsonObject();
+                int playlinkNum = episodeData.get("playlink_num").getAsInt();
+                if (playlinkNum == episode) {
+                    return episodeData.get("url").getAsString().split("\\?")[0]; // 返回剧集URL
+                }
+            }
+        }
+
+        // 如果未找到匹配的剧集，返回 null
+        return null;
+    }
+
+    public static void test() {
+        String xml = KanDanmuFetcher.getBilibiliDanmakuXML("北上广不相信眼泪", 1, 2015);
+        Logger.log(xml);
+    }
+}
