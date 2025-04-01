ad357f66117fe8a46152f9eb6dfbab9efea14964
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-04-02 07:21:18 +0800

    Update AListSh.java

--- app/src/main/java/com/github/catvod/spider/AListSh.java
+++ app/src/main/java/com/github/catvod/spider/AListSh.java
@@ -198,7 +198,7 @@ public class AListSh extends Spider {
         Thread thread = new Thread(() -> {
             Notify.show("开始构建本地索引，需要数秒");
             for (Drive d : drives) {
-                if (!d.search()) {
+                if (d.search()) {
                     d.exec("{ cat index.video.txt index.115.txt;echo ''; } > index.all.txt");
                     d.exec("{ cat index.video.txt index.115.txt;echo ''; } | awk -F '#' '{print $4,$0}' | sort -r | cut -d ' ' -f 2- > index.all.desc.txt");
                 }
