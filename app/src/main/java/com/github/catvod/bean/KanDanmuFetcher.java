c1d3c48c41a7a9684308270d8055d3ac2dd3e15a
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-04-01 10:32:22 +0800

    Update KanDanmuFetcher.java

--- app/src/main/java/com/github/catvod/bean/KanDanmuFetcher.java
+++ app/src/main/java/com/github/catvod/bean/KanDanmuFetcher.java
@@ -59,7 +59,7 @@ public class KanDanmuFetcher extends DanmuFetcher {
 
     private String searchEnId(String title, int year) throws IOException {
         // URL 编码影片名
-        String encodedTitle = URLEncoder.encode(title, StandardCharsets.UTF_8.toString());
+        String encodedTitle = URLEncoder.encode(title, "UTF-8");
         // 使用步骤1的API搜索节目ID
         String searchUrl = "https://api.so.360kan.com/index?force_v=1&from=&pageno=1&v_ap=1&tab=all&kw=" + encodedTitle;
         String jsonResponse = sendGetRequest(searchUrl);
