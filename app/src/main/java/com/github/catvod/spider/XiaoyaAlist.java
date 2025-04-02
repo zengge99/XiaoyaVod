41b7f7e6042cd6b4d350f5f267bb5d0fc462a6ba
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-04-02 09:09:20 +0800

    ok

--- app/src/main/java/com/github/catvod/spider/XiaoyaAlist.java
+++ app/src/main/java/com/github/catvod/spider/XiaoyaAlist.java
@@ -12,7 +12,7 @@ public class XiaoyaAlist extends Spider {
     @Override
     public void init(Context context, String extend) throws Exception {
         if (core == null) {
-            core = new AListSh();
+            core = new AList();
         }
         core.init(context, extend); 
     }
