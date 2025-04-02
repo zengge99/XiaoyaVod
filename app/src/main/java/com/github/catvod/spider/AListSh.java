1745adaad78903d23f1d6bca1a3ca3fd0f739cbf
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-04-02 16:47:13 +0800

    ok

--- app/src/main/java/com/github/catvod/spider/AListSh.java
+++ app/src/main/java/com/github/catvod/spider/AListSh.java
@@ -488,7 +488,8 @@ public class AListSh extends Spider {
         }
         int total = Integer.parseInt(drive.exec(cmd + " | wc -l").split("\n")[0]);
 
-        cmd += " | awk -F '#' 'length($5) {non_empty = non_empty $0 \"\\n\"; next} {empty = empty $0 \"\\n\"} END {printf \"%s%s\", non_empty, empty}'";
+        // cmd += " | awk -F '#' 'length($5) {non_empty = non_empty $0 \"\\n\"; next} {empty = empty $0 \"\\n\"} END {printf \"%s%s\", non_empty, empty}'";
+        cmd = "{ cmd | grep http; cmd | grep -v http; }"
 
         int limit = 72;
         int count = (total + limit - 1) / limit;
