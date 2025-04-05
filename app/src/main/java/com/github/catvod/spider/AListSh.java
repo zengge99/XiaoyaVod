f4ad0a7c9b6cff9a0a875ec9f7a5cb196c99d14b
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-04-05 10:58:53 +0800

    ok

--- app/src/main/java/com/github/catvod/spider/AListSh.java
+++ app/src/main/java/com/github/catvod/spider/AListSh.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Iterator;
+import org.json.JSONObject;
 
 public class AListSh extends AList {
     private boolean fallback = true;
