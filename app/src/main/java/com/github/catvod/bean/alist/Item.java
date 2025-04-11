a7221fab88b22d9af6bb3bfddcdb5564827d720b
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-04-11 10:15:34 +0800

    ok

--- app/src/main/java/com/github/catvod/bean/alist/Item.java
+++ app/src/main/java/com/github/catvod/bean/alist/Item.java
@@ -93,7 +93,7 @@ public class Item {
     }
 
     public String getModified() {
-        return TextUtils.isEmpty(modified) ? "" : modified;;
+        return TextUtils.isEmpty(modified) ? "" : modified;
     }
 
     public Date getDate() {
