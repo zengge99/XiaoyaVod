adc342721ff3c262c39f7eaf4db1576ec8c2f90b
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-04-20 10:43:16 +0800

    ok

--- app/src/main/java/com/github/catvod/spider/AListSh.java
+++ app/src/main/java/com/github/catvod/spider/AListSh.java
@@ -142,6 +142,29 @@ public class AListSh extends AList {
         yearFilterValues.add(new Filter.Value(String.valueOf(thisYear - 10) + "及以前", String.valueOf(thisYear - 10) + "-"));
         items.add(new Filter("year", "年份", yearFilterValues));
 
+        List<Filter.Value> typeFilterValues = new ArrayList<>();
+        List<String> typeList = Arrays.asList(
+            "喜剧", "爱情", "动作", "科幻", "动画", "悬疑", "犯罪", "惊悚", 
+            "冒险", "音乐", "历史", "奇幻", "恐怖", "战争", "传记", "歌舞", 
+            "武侠", "灾难", "西部", "纪录片", "短片", "剧情", "家庭", 
+            "儿童", "古装", "运动", "黑色电影");
+        for (String s : typeList) {
+            typeFilterValues.add(new Filter.Value(s, s));
+        }
+        items.add(new Filter("type", "类型", typeFilterValues));
+
+        List<Filter.Value> areaFilterValues = new ArrayList<>();
+        List<String> areaList = Arrays.asList(
+            "中国", "韩国", "日本", "美国", "欧美", "印度", "泰国");
+        for (String s : areaList) {
+            String v = s;
+            if (s.equals("欧美")) {
+                v = "(美国|英国|法国|德国|意大利|西班牙|瑞典|丹麦|爱尔兰|澳大利亚|巴西)"
+            }
+            areaFilterValues.add(new Filter.Value(s, v));
+        }
+        items.add(new Filter("area", "地区", areaFilterValues));
+
         items.addAll(super.getFilter(tid));
 
         return items;
