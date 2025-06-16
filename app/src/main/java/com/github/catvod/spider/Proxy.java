4b770c1f776f71a550876c3ad4b0631f78501da2
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-06-16 11:50:46 +0800

    ok

--- app/src/main/java/com/github/catvod/spider/Proxy.java
+++ app/src/main/java/com/github/catvod/spider/Proxy.java
@@ -5,14 +5,37 @@ import com.github.catvod.crawler.SpiderDebug;
 import com.github.catvod.net.OkHttp;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.util.Map;
+import com.github.catvod.utils.Path;
 
 public class Proxy extends Spider {
 
     private static int port = -1;
 
     public static Object[] proxy(Map<String, String> params) throws Exception {
-        return XiaoyaProxyHandler.proxy(params);
+        //return XiaoyaProxyHandler.proxy(params);
+        switch (params.get("do")) {
+            case "ck":
+                return new Object[]{200, "text/plain; charset=utf-8", new ByteArrayInputStream("ok".getBytes("UTF-8"))};
+            case "dbg":
+                Logger.dbg = true;
+                return new Object[]{200, "text/plain; charset=utf-8", new ByteArrayInputStream("ok".getBytes("UTF-8"))};
+            case "fs":
+                return readFile(params);
+            default:
+                return null;
+        }
+    }
+
+    private static Object[] readFile(Map<String, String> params) throws Exception {
+        String filePath = params.get("file");
+        if (!filePath.isEmpty()) {
+            File file = new File(filePath);
+            return new Object[]{200, "text/plain; charset=utf-8", new ByteArrayInputStream(Path.read(file).getBytes("UTF-8"))};
+        } else {
+            return null;
+        }
     }
 
     static void adjustPort() {
@@ -20,7 +43,7 @@ public class Proxy extends Spider {
         int port = 9978;
         while (port < 10000) {
             String resp = OkHttp.string("http://127.0.0.1:" + port + "/proxy?do=ck", null);
-            if (resp.equals("xiaoya_proxy_ok")) {
+            if (resp.equals("ok")) {
                 SpiderDebug.log("Found local server port " + port);
                 Proxy.port = port;
                 break;
