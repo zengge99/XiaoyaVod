a14039447b11029ebab1d8486cecacd243170919
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-07-21 10:23:42 +0800

    自然排序算法替换

--- app/src/main/java/com/github/catvod/bean/alist/Sorter.java
+++ app/src/main/java/com/github/catvod/bean/alist/Sorter.java
@@ -7,6 +7,7 @@ public class Sorter implements Comparator<Item> {
 
     private final String type;
     private final String order;
+    private NaturalSorter nameSorter = new NaturalSorter();
 
     public static void sort(String type, String order, List<Item> items) {
         Collections.sort(items, new Sorter(type, order));
@@ -23,17 +24,102 @@ public class Sorter implements Comparator<Item> {
         switch (type) {
             case "name":
                 //return asc ? o1.getName().compareTo(o2.getName()) : o2.getName().compareTo(o1.getName());
-                Map<String, Object> options = new HashMap<>();
-                options.put("order", order.equals("asc") ? order : "desc"); 
-                options.put("caseSensitive", false);
-                NaturalSort naturalSort = new NaturalSort(options);
-                return naturalSort.compare(o1, o2);
+                // Map<String, Object> options = new HashMap<>();
+                // options.put("order", order.equals("asc") ? order : "desc"); 
+                // options.put("caseSensitive", false);
+                // NaturalSort naturalSort = new NaturalSort(options);
+                // return naturalSort.compare(o1, o2);
+                int result = nameSorter.compare(o1, o2);
+                return order.equals("asc") ? result : -result;
             case "size":
                 return asc ? Long.compare(o1.getSize(), o2.getSize()) : Long.compare(o2.getSize(), o1.getSize());
             case "date":
                 return asc ? o1.getDate().compareTo(o2.getDate()) : o2.getDate().compareTo(o1.getDate());
             default:
                 return 0;
+        }
+    }
+
+    //互联网上找来的自然排序，更加易读懂
+    class NaturalSorter implements Comparator<String> {
+
+        @Override
+        public int compare(String o1, String o2) {
+            int index1 = 0;
+            int index2 = 0;
+            while (true) {
+            String data1 = nextSlice(o1, index1);
+            String data2 = nextSlice(o2, index2);
+
+            if (data1 == null && data2 == null) {
+                return 0;
+            }
+            if (data1 == null) {
+                return -1;
+            }
+            if (data2 == null) {
+                return 1;
+            }
+
+            index1 += data1.length();
+            index2 += data2.length();
+
+            int result;
+            if (isDigit(data1) && isDigit(data2)) {
+                result = Long.compare(Long.valueOf(data1), Long.valueOf(data2));
+                if (result == 0) {
+                result = -Integer.compare(data1.length(), data2.length());
+                }
+            } else {
+                result = data1.compareToIgnoreCase(data2);
+            }
+
+            if (result != 0) {
+                return result;
+            }
+            }
+        }
+
+        private static boolean isDigit(String str) {
+            // Just check the first char
+            char ch = str.charAt(0);
+            return ch >= '0' && ch <= '9';
+        }
+
+        static String nextSlice(String str, int index) {
+            int length = str.length();
+            if (index == length) {
+            return null;
+            }
+
+            char ch = str.charAt(index);
+            if (ch == '.' || ch == ' ') {
+            return str.substring(index, index + 1);
+            } else if (ch >= '0' && ch <= '9') {
+            return str.substring(index, nextNumberBound(str, index + 1));
+            } else {
+            return str.substring(index, nextOtherBound(str, index + 1));
+            }
+        }
+
+        private static int nextNumberBound(String str, int index) {
+            for (int length = str.length(); index < length; index++) {
+            char ch = str.charAt(index);
+            if (ch < '0' || ch > '9') {
+                break;
+            }
+            }
+            return index;
+        }
+
+        private static int nextOtherBound(String str, int index) {
+            for (int length = str.length(); index < length; index++) {
+            char ch = str.charAt(index);
+            if (ch == '.' || ch == ' ' || (ch >= '0' && ch <= '9')) {
+                break;
+            }
+            }
+            return index;
         }
     }
 
