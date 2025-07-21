b2124ca0ef240b720ae4d47dee0fd0404f15c7be
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-07-21 11:53:17 +0800

    ok

--- app/src/main/java/com/github/catvod/bean/alist/Sorter.java
+++ app/src/main/java/com/github/catvod/bean/alist/Sorter.java
@@ -49,7 +49,7 @@ public class Sorter implements Comparator<Item> {
             int index1 = 0;
             int index2 = 0;
             while (true) {
-                String data1 = nextSlice(o1, index1)的
+                String data1 = nextSlice(o1, index1);
                 String data2 = nextSlice(o2, index2);
 
                 if (data1 == null && data2 == null) {
