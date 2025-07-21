ad6f4ac4a531b22ea4d14c43a609da640e66e352
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-07-21 10:43:01 +0800

    ok

--- app/src/main/java/com/github/catvod/bean/alist/Sorter.java
+++ app/src/main/java/com/github/catvod/bean/alist/Sorter.java
@@ -29,7 +29,7 @@ public class Sorter implements Comparator<Item> {
                 // options.put("caseSensitive", false);
                 // NaturalSort naturalSort = new NaturalSort(options);
                 // return naturalSort.compare(o1, o2);
-                return asc? nameSorter.compare(o1.getName(), o2.getName()) : nameSorter.compare(o2.getName(), o1.getName());
+                return asc ? nameSorter.compare(o1.getName(), o2.getName()) : nameSorter.compare(o2.getName(), o1.getName());
             case "size":
                 return asc ? Long.compare(o1.getSize(), o2.getSize()) : Long.compare(o2.getSize(), o1.getSize());
             case "date":
@@ -47,35 +47,35 @@ public class Sorter implements Comparator<Item> {
             int index1 = 0;
             int index2 = 0;
             while (true) {
-            String data1 = nextSlice(o1, index1);
-            String data2 = nextSlice(o2, index2);
+                String data1 = nextSlice(o1, index1);
+                String data2 = nextSlice(o2, index2);
 
-            if (data1 == null && data2 == null) {
-                return 0;
-            }
-            if (data1 == null) {
-                return -1;
-            }
-            if (data2 == null) {
-                return 1;
-            }
-
-            index1 += data1.length();
-            index2 += data2.length();
-
-            int result;
-            if (isDigit(data1) && isDigit(data2)) {
-                result = Long.compare(Long.valueOf(data1), Long.valueOf(data2));
-                if (result == 0) {
-                result = -Integer.compare(data1.length(), data2.length());
+                if (data1 == null && data2 == null) {
+                    return 0;
                 }
-            } else {
-                result = data1.compareToIgnoreCase(data2);
-            }
+                if (data1 == null) {
+                    return -1;
+                }
+                if (data2 == null) {
+                    return 1;
+                }
 
-            if (result != 0) {
-                return result;
-            }
+                index1 += data1.length();
+                index2 += data2.length();
+
+                int result;
+                if (isDigit(data1) && isDigit(data2)) {
+                    result = Long.compare(Long.valueOf(data1), Long.valueOf(data2));
+                    if (result == 0) {
+                        result = -Integer.compare(data1.length(), data2.length());
+                    }
+                } else {
+                    result = data1.compareToIgnoreCase(data2);
+                }
+
+                if (result != 0) {
+                    return result;
+                }
             }
         }
 
@@ -88,35 +88,35 @@ public class Sorter implements Comparator<Item> {
         static String nextSlice(String str, int index) {
             int length = str.length();
             if (index == length) {
-            return null;
+                return null;
             }
 
             char ch = str.charAt(index);
             if (ch == '.' || ch == ' ') {
-            return str.substring(index, index + 1);
+                return str.substring(index, index + 1);
             } else if (ch >= '0' && ch <= '9') {
-            return str.substring(index, nextNumberBound(str, index + 1));
+                return str.substring(index, nextNumberBound(str, index + 1));
             } else {
-            return str.substring(index, nextOtherBound(str, index + 1));
+                return str.substring(index, nextOtherBound(str, index + 1));
             }
         }
 
         private static int nextNumberBound(String str, int index) {
             for (int length = str.length(); index < length; index++) {
-            char ch = str.charAt(index);
-            if (ch < '0' || ch > '9') {
-                break;
-            }
+                char ch = str.charAt(index);
+                if (ch < '0' || ch > '9') {
+                    break;
+                }
             }
             return index;
         }
 
         private static int nextOtherBound(String str, int index) {
             for (int length = str.length(); index < length; index++) {
-            char ch = str.charAt(index);
-            if (ch == '.' || ch == ' ' || (ch >= '0' && ch <= '9')) {
-                break;
-            }
+                char ch = str.charAt(index);
+                if (ch == '.' || ch == ' ' || (ch >= '0' && ch <= '9')) {
+                    break;
+                }
             }
             return index;
         }
