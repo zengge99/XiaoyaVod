2deb288573e97d3b813aa00e58a2d888c157fbf9
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-07-21 10:48:25 +0800

    ok

--- app/src/main/java/com/github/catvod/bean/alist/Sorter.java
+++ app/src/main/java/com/github/catvod/bean/alist/Sorter.java
@@ -123,121 +123,121 @@ public class Sorter implements Comparator<Item> {
     }
 
     //从alist.min.js移植的自然排序
-    class NaturalSort {
-        private final Map<String, Object> options;
+    // class NaturalSort {
+    //     private final Map<String, Object> options;
 
-        public NaturalSort(Map<String, Object> options) {
-            this.options = options != null ? options : new HashMap<>();
-        }
+    //     public NaturalSort(Map<String, Object> options) {
+    //         this.options = options != null ? options : new HashMap<>();
+    //     }
 
-        public int compare(Item a, Item b) {
-            final int EQUAL = 0;
-            final int GREATER = "desc".equals(options.get("order")) ? -1 : 1;
-            final int SMALLER = -GREATER;
+    //     public int compare(Item a, Item b) {
+    //         final int EQUAL = 0;
+    //         final int GREATER = "desc".equals(options.get("order")) ? -1 : 1;
+    //         final int SMALLER = -GREATER;
 
-            String aValue = a.getName();
-            String bValue = b.getName();
+    //         String aValue = a.getName();
+    //         String bValue = b.getName();
 
-            Pattern re = Pattern.compile("(^-?[0-9]+(\\.[0-9]*)?[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)", Pattern.CASE_INSENSITIVE);
-            Pattern sre = Pattern.compile("(^[ ]*|[ ]*$)");
-            Pattern dre = Pattern.compile("(^([\\w ]+,?[\\w ]+)?[\\w ]+,?[\\w ]+\\d+:\\d+(:\\d+)?[\\w ]?|^\\d{1,4}[/-]\\d{1,4}[/-]\\d{1,4}|^\\w+, \\w+ \\d+, \\d{4})");
-            Pattern hre = Pattern.compile("^0x[0-9a-f]+$", Pattern.CASE_INSENSITIVE);
-            Pattern ore = Pattern.compile("^0");
+    //         Pattern re = Pattern.compile("(^-?[0-9]+(\\.[0-9]*)?[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)", Pattern.CASE_INSENSITIVE);
+    //         Pattern sre = Pattern.compile("(^[ ]*|[ ]*$)");
+    //         Pattern dre = Pattern.compile("(^([\\w ]+,?[\\w ]+)?[\\w ]+,?[\\w ]+\\d+:\\d+(:\\d+)?[\\w ]?|^\\d{1,4}[/-]\\d{1,4}[/-]\\d{1,4}|^\\w+, \\w+ \\d+, \\d{4})");
+    //         Pattern hre = Pattern.compile("^0x[0-9a-f]+$", Pattern.CASE_INSENSITIVE);
+    //         Pattern ore = Pattern.compile("^0");
 
-            String x = normalize(aValue).replaceAll(sre.pattern(), "");
-            String y = normalize(bValue).replaceAll(sre.pattern(), "");
+    //         String x = normalize(aValue).replaceAll(sre.pattern(), "");
+    //         String y = normalize(bValue).replaceAll(sre.pattern(), "");
 
-            String[] xN = x.replaceAll(re.pattern(), "\0$1\0").replaceAll("\0$", "").replaceAll("^\0", "").split("\0");
-            String[] yN = y.replaceAll(re.pattern(), "\0$1\0").replaceAll("\0$", "").replaceAll("^\0", "").split("\0");
+    //         String[] xN = x.replaceAll(re.pattern(), "\0$1\0").replaceAll("\0$", "").replaceAll("^\0", "").split("\0");
+    //         String[] yN = y.replaceAll(re.pattern(), "\0$1\0").replaceAll("\0$", "").replaceAll("^\0", "").split("\0");
 
-            if (x.isEmpty() && y.isEmpty()) return EQUAL;
-            if (x.isEmpty() && !y.isEmpty()) return GREATER;
-            if (!x.isEmpty() && y.isEmpty()) return SMALLER;
+    //         if (x.isEmpty() && y.isEmpty()) return EQUAL;
+    //         if (x.isEmpty() && !y.isEmpty()) return GREATER;
+    //         if (!x.isEmpty() && y.isEmpty()) return SMALLER;
 
-            Integer xD = null;
-            Integer yD = null;
+    //         Integer xD = null;
+    //         Integer yD = null;
 
-            Matcher xHreMatcher = hre.matcher(x);
-            Matcher yHreMatcher = hre.matcher(y);
-            Matcher xDreMatcher = dre.matcher(x);
-            Matcher yDreMatcher = dre.matcher(y);
+    //         Matcher xHreMatcher = hre.matcher(x);
+    //         Matcher yHreMatcher = hre.matcher(y);
+    //         Matcher xDreMatcher = dre.matcher(x);
+    //         Matcher yDreMatcher = dre.matcher(y);
 
-            if (xHreMatcher.find()) {
-                xD = Integer.parseInt(x.substring(xHreMatcher.start(), xHreMatcher.end()), 16);
-            } else if (xN.length != 1 && xDreMatcher.find()) {
-                try {
-                    xD = (int) (new Date(x).getTime() / 1000);
-                } catch (Exception e) {
-                    xD = null;
-                }
-            }
+    //         if (xHreMatcher.find()) {
+    //             xD = Integer.parseInt(x.substring(xHreMatcher.start(), xHreMatcher.end()), 16);
+    //         } else if (xN.length != 1 && xDreMatcher.find()) {
+    //             try {
+    //                 xD = (int) (new Date(x).getTime() / 1000);
+    //             } catch (Exception e) {
+    //                 xD = null;
+    //             }
+    //         }
 
-            if (yHreMatcher.find()) {
-                yD = Integer.parseInt(y.substring(yHreMatcher.start(), yHreMatcher.end()), 16);
-            } else if (xD != null && yDreMatcher.find()) {
-                try {
-                    yD = (int) (new Date(y).getTime() / 1000);
-                } catch (Exception e) {
-                    yD = null;
-                }
-            } else {
-                yD = null;
-            }
+    //         if (yHreMatcher.find()) {
+    //             yD = Integer.parseInt(y.substring(yHreMatcher.start(), yHreMatcher.end()), 16);
+    //         } else if (xD != null && yDreMatcher.find()) {
+    //             try {
+    //                 yD = (int) (new Date(y).getTime() / 1000);
+    //             } catch (Exception e) {
+    //                 yD = null;
+    //             }
+    //         } else {
+    //             yD = null;
+    //         }
 
-            if (yD != null) {
-                if (xD != null && xD < yD) return SMALLER;
-                else if (xD != null && xD > yD) return GREATER;
-            }
+    //         if (yD != null) {
+    //             if (xD != null && xD < yD) return SMALLER;
+    //             else if (xD != null && xD > yD) return GREATER;
+    //         }
 
-            for (int cLoc = 0, numS = Math.max(xN.length, yN.length); cLoc < numS; cLoc++) {
-                String xNcL = cLoc < xN.length ? xN[cLoc] : "";
-                String yNcL = cLoc < yN.length ? yN[cLoc] : "";
+    //         for (int cLoc = 0, numS = Math.max(xN.length, yN.length); cLoc < numS; cLoc++) {
+    //             String xNcL = cLoc < xN.length ? xN[cLoc] : "";
+    //             String yNcL = cLoc < yN.length ? yN[cLoc] : "";
 
-                Object oFxNcL;
-                Object oFyNcL;
+    //             Object oFxNcL;
+    //             Object oFyNcL;
 
-                if (!ore.matcher(xNcL).find()) {
-                    try {
-                        oFxNcL = Double.parseDouble(xNcL);
-                    } catch (NumberFormatException e) {
-                        oFxNcL = xNcL.isEmpty() ? 0 : xNcL;
-                    }
-                } else {
-                    oFxNcL = xNcL.isEmpty() ? 0 : xNcL;
-                }
+    //             if (!ore.matcher(xNcL).find()) {
+    //                 try {
+    //                     oFxNcL = Double.parseDouble(xNcL);
+    //                 } catch (NumberFormatException e) {
+    //                     oFxNcL = xNcL.isEmpty() ? 0 : xNcL;
+    //                 }
+    //             } else {
+    //                 oFxNcL = xNcL.isEmpty() ? 0 : xNcL;
+    //             }
 
-                if (!ore.matcher(yNcL).find()) {
-                    try {
-                        oFyNcL = Double.parseDouble(yNcL);
-                    } catch (NumberFormatException e) {
-                        oFyNcL = yNcL.isEmpty() ? 0 : yNcL;
-                    }
-                } else {
-                    oFyNcL = yNcL.isEmpty() ? 0 : yNcL;
-                }
+    //             if (!ore.matcher(yNcL).find()) {
+    //                 try {
+    //                     oFyNcL = Double.parseDouble(yNcL);
+    //                 } catch (NumberFormatException e) {
+    //                     oFyNcL = yNcL.isEmpty() ? 0 : yNcL;
+    //                 }
+    //             } else {
+    //                 oFyNcL = yNcL.isEmpty() ? 0 : yNcL;
+    //             }
 
-                if ((oFxNcL instanceof Double && Double.isNaN((Double) oFxNcL)) != 
-                    (oFyNcL instanceof Double && Double.isNaN((Double) oFyNcL))) {
-                    return (oFxNcL instanceof Double && Double.isNaN((Double) oFxNcL)) ? GREATER : SMALLER;
-                } else if (oFxNcL.getClass() != oFyNcL.getClass()) {
-                    oFxNcL = oFxNcL.toString();
-                    oFyNcL = oFyNcL.toString();
-                }
+    //             if ((oFxNcL instanceof Double && Double.isNaN((Double) oFxNcL)) != 
+    //                 (oFyNcL instanceof Double && Double.isNaN((Double) oFyNcL))) {
+    //                 return (oFxNcL instanceof Double && Double.isNaN((Double) oFxNcL)) ? GREATER : SMALLER;
+    //             } else if (oFxNcL.getClass() != oFyNcL.getClass()) {
+    //                 oFxNcL = oFxNcL.toString();
+    //                 oFyNcL = oFyNcL.toString();
+    //             }
 
-                if (oFxNcL instanceof Comparable && oFyNcL instanceof Comparable) {
-                    @SuppressWarnings("unchecked")
-                    int compareResult = ((Comparable<Object>) oFxNcL).compareTo(oFyNcL);
-                    if (compareResult < 0) return SMALLER;
-                    if (compareResult > 0) return GREATER;
-                }
-            }
+    //             if (oFxNcL instanceof Comparable && oFyNcL instanceof Comparable) {
+    //                 @SuppressWarnings("unchecked")
+    //                 int compareResult = ((Comparable<Object>) oFxNcL).compareTo(oFyNcL);
+    //                 if (compareResult < 0) return SMALLER;
+    //                 if (compareResult > 0) return GREATER;
+    //             }
+    //         }
 
-            return EQUAL;
-        }
+    //         return EQUAL;
+    //     }
 
-        private String normalize(String value) {
-            String string = String.valueOf(value);
-            return Boolean.TRUE.equals(options.get("caseSensitive")) ? string : string.toLowerCase();
-        }
-    }
+    //     private String normalize(String value) {
+    //         String string = String.valueOf(value);
+    //         return Boolean.TRUE.equals(options.get("caseSensitive")) ? string : string.toLowerCase();
+    //     }
+    // }
 }
