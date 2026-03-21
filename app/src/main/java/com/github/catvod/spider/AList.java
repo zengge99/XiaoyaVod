47f6aa49107f141b07f89f42a60eb7ec8b7e651a
zengge99 <83830807+zengge99@users.noreply.github.com>
2026-03-21 18:11:00 +0800

    ok

--- app/src/main/java/com/github/catvod/spider/AList.java
+++ app/src/main/java/com/github/catvod/spider/AList.java
@@ -570,7 +570,7 @@ public class AList extends Spider {
                 }
 
                 for (int i = 0; i < displayPaths.size(); i++) {
-                    displayPaths[i] = String.format("%d: %s", i, displayPaths[i]);
+                    displayPaths.set(i, String.format("%d: %s", i + 1, displayPaths.get(i)));
                 }
                 
                 String displayPlot = vod.doubanInfo.getId().isEmpty() ? "文件路径: \r\n" + TextUtils.join("\r\n", displayPaths) : vod.doubanInfo.getPlot() + "\r\n\r\n文件路径: \r\n" + TextUtils.join("\r\n", displayPaths);
