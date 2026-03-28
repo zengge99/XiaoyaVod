5dfa33ffc9f9214d9e8091aa9e64ea53a0a5013f
zengge99 <83830807+zengge99@users.noreply.github.com>
2026-03-28 09:44:23 +0800

    ok

--- app/src/main/java/com/github/catvod/bean/DanmuFetcher.java
+++ app/src/main/java/com/github/catvod/bean/DanmuFetcher.java
@@ -22,6 +22,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.OutputStream;
 
 public class DanmuFetcher {
     private static final DanmuFetcher INSTANCE = new DanmuFetcher();
