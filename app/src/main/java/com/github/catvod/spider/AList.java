3147a36916eb39b957b49855afa64e68fbbfc20d
zengge99 <83830807+zengge99@users.noreply.github.com>
2026-04-14 20:18:37 +0800

    Update AList.java

--- app/src/main/java/com/github/catvod/spider/AList.java
+++ app/src/main/java/com/github/catvod/spider/AList.java
@@ -1213,7 +1213,7 @@ public static List<String> doFilter(LocalIndexService service, HashMap<String, S
                 vod.setVodYear(item.doubanInfo.getYear());
                 vod.doubanInfo = item.doubanInfo;
                 vod.setVodId(vod.getVodId() + "/~xiaoya");
-                if (TextUtils.isEmpty(item.getThumb())) {
+                if (TextUtils.isEmpty(item.getThumb()) || TextUtils.isEmpty(item.getId())) {
                     vod.setVodPic(Image.XIAOYA);
                     noPicList.add(vod);
                 } else {
